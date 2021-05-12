ec39d243113cf269faf5c5eee6f4d71ed96499f2
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-05-12 08:26:51 -0300

    feat: Implementar beforeEach e teste Deve retornar o número de IDs quando getNumberOfGeneratedIds for chamado

--- src/services/unique-id/unique-id.service.spec.ts
+++ src/services/unique-id/unique-id.service.spec.ts
@@ -14,21 +14,32 @@
 import { UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
+
+  let service: UniqueIdService = null;
+
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
-    // const firstId = service.generateUniqueIdWithPrefix('app');
-    // const secondId = service.generateUniqueIdWithPrefix('app');
-    // expect(firstId).not.toBe(secondId);
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should not generate duplicate IDs when called multiple times`, () => {
     const ids = new Set();
     for(let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
     }
     expect(ids.size).toBe(50);
   });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name} 
+    should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedIds()).toBe(2);
+  })
 });
