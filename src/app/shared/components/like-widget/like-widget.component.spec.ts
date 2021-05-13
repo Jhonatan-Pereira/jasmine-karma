d96bbca75cacfef0ff6c6b07f5344a9309a12eef
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-05-13 11:25:39 -0300

    refact: Refatorar o nome dos testes

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -20,12 +20,12 @@ describe(LikeWidgetComponent.name, () => {
   });
 
   
-  it('Should auto generate ID when id input property is missing', () => {
+  it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
     fixture.detectChanges();
     expect(component.id).toBeTruthy();
   });
 
-  it('Should NOT generate ID when id input property is present', () => {
+  it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
     const someID = 'someId';
     component.id = someID;
     fixture.detectChanges();
@@ -33,15 +33,10 @@ describe(LikeWidgetComponent.name, () => {
   });
 
   it(`#${LikeWidgetComponent.prototype.like.name}
-    Should trigger emission then called`, () => {
+    Should trigger (@Output liked) then called`, () => {
       spyOn(component.liked, 'emit');
       fixture.detectChanges();
       component.like();
       expect(component.liked.emit).toHaveBeenCalled();
-      // component.liked.subscribe(() => {
-      //   expect(true).toBeTrue();
-      //   done();
-      // });
-      // component.like();
     });
 });
