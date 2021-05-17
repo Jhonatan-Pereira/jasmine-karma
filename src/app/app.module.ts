5fef18d94ab4f21365f1e39e1b51eade7953e62a
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-05-16 23:28:19 -0300

    feat: Criado template photo-board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.mo
     BrowserModule,
     AppRoutingModule,
     LikeWidgetModule,
-    PhotoFrameModule
+    PhotoFrameModule,
+    PhotoBoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
