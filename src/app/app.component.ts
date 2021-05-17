46a37742e6df93471e5bf744b1001d0ada9574ac
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-05-16 23:48:01 -0300

    feat: Implementado componente app-photo-board no app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public likes = 0;
+export class AppComponent implements OnInit {
 
-  public like(): void {
-    this.likes++;
+  public photos$: Observable<Photo[]>;
+  
+  constructor(private service: PhotoBoardService) {}
+
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
+
 }
