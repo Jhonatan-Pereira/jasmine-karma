769cf19256b3d9029f5256afb19aac290019fe0d
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-05-17 09:06:03 -0300

    feat: Criado component photo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
-import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
-import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
+import { PhotoListModule } from './components/photo-list/photo-list.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +15,8 @@ import { PhotoBoardModule } from './shared/components/photo-board/photo-board.mo
     BrowserModule,
     AppRoutingModule,
     LikeWidgetModule,
-    PhotoFrameModule,
-    PhotoBoardModule,
-    HttpClientModule
+    HttpClientModule,
+    PhotoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
